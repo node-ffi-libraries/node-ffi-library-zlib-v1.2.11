febd0c1306ee6e27f75d8adcbce72be627af35ee
Joel Purra <mig@joelpurra.se>
2021-06-07 20:05:55 +0200

    Automatially generated package update
    - Based on `zlib` v1.2.11 from `conan` remote `conan-center`.
    - Generated by `@ffi-packager/ffi-packager` v2.0.0 (`v1.0.0-26-ge2367df` on `feature/ffi-generate-v2`).
    
    See
    
    - https://github.com/node-ffi-packager

---
+++ platforms/macos-armv8/include/zconf.h.js
@@ -0,0 +1,37 @@
+/*
+ * This file was automatically generated. It is better to run the generator again, than to manually edit.
+ *
+ * @ffi-packager/ffi-generate v2.0.1
+ * - git commit v2.0.1-1-g9e27804 on branch develop
+ * - clang version 9.0.1-16
+ * https://github.com/node-ffi-packager/node-ffi-generate
+ *
+ * File:
+ * - Name: "zconf.h"
+ * - SHA256: 289c8044a0e13ce2ea2183fe899f65c7ed8d091030b9c056ae6acdc39a8e1405
+ *
+ * Generator options:
+ * - Library: null
+ * - Single file: true
+ * - Prefixes: []
+ * - Compiler arguments: []
+ */
+
+const FFI = require("ffi-napi");
+const ref = require("ref-napi");
+const ArrayType = require("ref-array-di")(ref);
+const Struct = require("ref-struct-di")(ref);
+const Union = require("ref-union-di")(ref);
+
+const constants = {};
+
+// NOTE: defining individual types as "global" constants to be able to reference them without any prefix, but also under the "types" object to be able to reference them recursively.
+const types = {};
+
+const functions = new FFI.Library(null, {});
+
+module.exports = {
+  constants,
+  types,
+  functions,
+};
---
+++ platforms/windows-x86_64/include/zconf.h.js
@@ -0,0 +1,37 @@
+/*
+ * This file was automatically generated. It is better to run the generator again, than to manually edit.
+ *
+ * @ffi-packager/ffi-generate v2.0.1
+ * - git commit v2.0.1-1-g9e27804 on branch develop
+ * - clang version 9.0.1-16
+ * https://github.com/node-ffi-packager/node-ffi-generate
+ *
+ * File:
+ * - Name: "zconf.h"
+ * - SHA256: 9c8e959597037ca89f4d90ed79415258727b5c5f2ea9571f5d596ccf47ce0544
+ *
+ * Generator options:
+ * - Library: null
+ * - Single file: true
+ * - Prefixes: []
+ * - Compiler arguments: []
+ */
+
+const FFI = require("ffi-napi");
+const ref = require("ref-napi");
+const ArrayType = require("ref-array-di")(ref);
+const Struct = require("ref-struct-di")(ref);
+const Union = require("ref-union-di")(ref);
+
+const constants = {};
+
+// NOTE: defining individual types as "global" constants to be able to reference them without any prefix, but also under the "types" object to be able to reference them recursively.
+const types = {};
+
+const functions = new FFI.Library(null, {});
+
+module.exports = {
+  constants,
+  types,
+  functions,
+};
